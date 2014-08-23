04d4f86657f5487ebef36659ae7b06bc93ad8d41
glormph <jorrit.boekel@scilifelab.se>
2014-08-23 21:37:42 +0200

    Adapted to sr.se API v2. Works on xbmc 13.2 on rasbmc

--- default.py
+++ default.py
@@ -1,5 +1,7 @@
 # coding: latin-1                                                                                                      
+import os
 import urllib2
+from urlparse import urlsplit, urlunsplit, SplitResult
 
 import xbmcgui
 import xbmcplugin
@@ -7,9 +9,12 @@ import xbmcaddon
 
 from xml.dom.minidom import parse, parseString
 
-CHANNEL_URL = "http://api.sr.se/api/channels/channels.aspx"
-PROGRAM_LIST_URL = "http://api.sr.se/api/program/programfeed.aspx"
-PROGRAM_DETAIL_URL = "http://api.sr.se/api/program/broadcastfeed.aspx?unitid="
+BASE_API = "http://api.sr.se/api/v2/"
+CHANNEL_URL = "http://api.sr.se/api/v2/channels"
+PROGRAM_LIST_URL = "http://api.sr.se/api/v2/programs"
+BROADCAST_LIST_URL = "http://api.sr.se/api/v2/broadcasts?programid={0}&pagination=false"
+PROGRAM_DETAIL_URL = "http://api.sr.se/api/v2/program/broadcastfeed.aspx?unitid="
+PROGRAM_DETAIL_URL = "http://api.sr.se/api/radio/radio.aspx?type=broadcast&id={0}&codingformat=.m4a&metafile=m3u&quality={1}"
 BASE_URL = "http://sr.se"
 
 __settings__ = xbmcaddon.Addon(id='plugin.audio.sverigesradio')
@@ -20,52 +25,86 @@ PROTOCOLS = ["http", "rtsp"]
 SET_QUALITY = QUALITIES[int(__settings__.getSetting("quality"))]
 SET_PROTOCOL = PROTOCOLS[int(__settings__.getSetting("protocol"))]
 
-def list_channels():
+
+def fetch_channels():
     doc, state = load_xml(CHANNEL_URL)
     if doc and not state:
         for channel in doc.getElementsByTagName("channel"):
+            channel_id = channel.getAttribute("id")
             title = channel.getAttribute("name").encode('utf_8')
             originaltitle = title
-            logo = get_node_value(channel, "logo")
+            logo = get_node_value(channel, "image")
             description = get_node_value(channel, "tagline")
-            if description:
+            if description is not None:
                 description = description.encode('utf_8')
                 title = title + " - " + description
             else:
                 description = ''
-            streamingurl = channel.getElementsByTagName("streamingurl")
-            for url in streamingurl[0].getElementsByTagName("url"):
-                type = url.getAttribute("type")
-                protocol = url.getAttribute("protocol")
-                quality = url.getAttribute("quality")
-                playlist = url.getAttribute("playlist")
-                stream = url.childNodes[0].data
-                if type == "m4a" and quality == SET_QUALITY:
-                    if logo:
-                        add_posts(title, stream, description, logo, isLive = 'true', album = originaltitle, artist = 'Sveriges Radio')
-                    else:
-                        add_posts(title, stream, description, isLive = 'true', album = originaltitle, artist = 'Sveriges Radio')
+            audio_link = channel.getElementsByTagName("liveaudio")
+            streamingurl = audio_link[0].getElementsByTagName("url")[0].firstChild.nodeValue
+            yield {'id': channel_id,
+                   'title': title,
+                   'originaltitle': originaltitle,
+                   'logo': logo,
+                   'desc': description,
+                   'stream': streamingurl
+                   }
     else:
         if state == "site":
             xbmc.executebuiltin('Notification("Sveriges Radio","Site down")')
         else:
             xbmc.executebuiltin('Notification("Sveriges Radio","Malformed result")')
+
+
+def get_live(channel):
+    use_quality = {'high': 'hi',
+                   'low': 'low',
+                   'normal': 'normal',}[SET_QUALITY]
+    stream = urlsplit(channel['stream'])
+    protocol = stream.scheme
+    stream_dir, stream_file = os.path.split(stream.path)
+    new_stream_file = '-{0}'.format(use_quality).join(os.path.splitext(stream_file))
+    new_path = os.path.join(stream_dir, new_stream_file)
+    return {
+    'url': urlunsplit(SplitResult(protocol, stream.netloc, new_path, stream.query, stream.fragment)),
+    'logo': channel['logo'] if channel['logo'] is not None else '',}
+    
+def list_live():
+    for channel in fetch_channels():
+        live = get_live(channel)
+        add_posts(channel['title'], live['url'], channel['desc'], live['logo'], isLive = 'true', album=channel['originaltitle'], artist='Sveriges Radio')
     xbmcplugin.endOfDirectory(HANDLE)
 
-def list_programs(url):
-    doc, state = load_xml(PROGRAM_LIST_URL)
+
+def list_channels(channelsurl):
+    for channel in fetch_channels():
+        url = urlsplit(channelsurl)
+        new_path = os.path.join(url.path, 'programs', channel['id'])
+        new_url = urlunsplit(SplitResult(url.scheme, url.netloc, new_path, url.query, url.fragment))
+        print url, new_path, new_url
+        logo = channel['logo'] if channel['logo'] is not None else ''
+        add_posts(channel['title'],  new_url, channel['desc'], logo, isFolder = True, album = channel['originaltitle'], artist = 'Sveriges Radio')
+    xbmcplugin.endOfDirectory(HANDLE)
+        
+
+def list_channel_programs(url):
+    plisturl = urlsplit(PROGRAM_LIST_URL)
+    new_path = os.path.join(plisturl.path, 'index')
+    channel_id = os.path.basename(urlsplit(url).path)
+    programs_url = urlunsplit(SplitResult(plisturl.scheme, plisturl.netloc, new_path, 'channelid={0}'.format(channel_id), ''))
+    doc, state = load_xml(programs_url)
     if doc and not state:
-        for program in doc.getElementsByTagName("item"):
-            title = get_node_value(program, "title").encode('utf_8')
+        for program in doc.getElementsByTagName("program"):
+            program_id = program.getAttribute("id")
+            title = program.getAttribute("name").encode('utf_8')
             description = get_node_value(program, "description")
             if description:
                 description = description.encode('utf_8')
                 title = title + " - " + description
             else:
                 description = ''
-            unitid = ""
-            unitid = get_node_value(program, "unitid").encode('utf_8')
-            add_posts(title, url + unitid + "/", isFolder=True)
+            logo = get_node_value(program, "programimage")
+            add_posts(title, url + '/program/' + program_id + "/", description, logo, isFolder=True)
     else:
         if state == "site":
             xbmc.executebuiltin('Notification("Sveriges Radio","Site down")')
@@ -73,45 +112,20 @@ def list_programs(url):
             xbmc.executebuiltin('Notification("Sveriges Radio","Malformed result")')
     xbmcplugin.endOfDirectory(HANDLE)
 
-def list_program(unitid):
-    doc, state = load_xml(PROGRAM_DETAIL_URL+unitid)
+
+def list_broadcasts(program_id):
+    doc, state = load_xml(BROADCAST_LIST_URL.format(program_id))
     if doc and not state:
-        urlset = doc.getElementsByTagName("urlset")[0]
         base = ""                                                                         
-        for url in urlset.getElementsByTagName("url"):
-            type = url.getAttribute("type")
-            protocol = url.getAttribute("protocol")
-            quality = url.getAttribute("quality")
-            if type == "m4a" and protocol == SET_PROTOCOL and quality == SET_QUALITY:
-                base = url.childNodes[0].data
-        if base == "":
-            base = "http://sverigesradio.se/topsy/ljudfil/utan/statistik/[broadcastid].m4a"
-            xbmc.log("Couldn't find url template, go with known (" + base + ")")
-        for audio in doc.getElementsByTagName("item"):
-            for ondemand in audio.getElementsByTagName("ondemand"):
-                title = ondemand.getAttribute("mainbroadcasttitle").encode('utf_8')
-                originaltitle = title
-                description = ondemand.getAttribute("mainbroadcastdescription").encode('utf_8')
-                if description:
-                    title = description
-                else:
-                    description = ''
-                date = ondemand.getAttribute("mainbroadcastdate").encode('utf_8')
-                if date:
-                    title = title + " - " + date
-                else:
-                    date = ''
-                thumb = ondemand.getAttribute("image").encode('utf_8')
-                if thumb:
-                    thumb = BASE_URL + thumb
-                else:
-                    thumb = ''
-                for broadcast in ondemand.getElementsByTagName("broadcastfilename"):
-                    broadcastid = broadcast.getAttribute("broadcastid").encode('utf_8')                               
-                    broadcastname = broadcast.childNodes[0].data.encode('utf_8')
-                    url = base.replace("[broadcastid]", broadcastid)
-                    url = url.replace("[broadcastfilename]", broadcastname)
-                    add_posts(title, url, description, thumb, artist='Sveriges Radio', album=originaltitle)
+        for broadcast in doc.getElementsByTagName("broadcast"):
+            title = get_node_value(broadcast, "title")
+            duration = get_node_value(broadcast, "totalduration")
+            bcastfiles = broadcast.getElementsByTagName("broadcastfile")
+            url = get_node_value(bcastfiles[0], "url")
+            logo = get_node_value(broadcast, "image")
+            logourl = BASE_URL + logo
+            description = get_node_value(broadcast, "description")
+            add_posts(title, url, description, logourl, duration=duration) #url, description, thumb, artist='Sveriges Radio', album=originaltitle, duration=duration)
     else:
         if state == "site":
             xbmc.executebuiltin('Notification("Sveriges Radio","Site down")')
@@ -120,26 +134,32 @@ def list_program(unitid):
     xbmcplugin.endOfDirectory(HANDLE)
 
 
-def add_posts(title, url, description='', thumb='', isPlayable='true', isLive='false', isFolder=False, artist='',\
-              album=''):
+def add_posts(title, url, description='', thumb='', isPlayable='true', isLive='false', isFolder=False, artist='',
+              album='', duration=''):
+    print 'duration is', duration
     title = title.replace("\n", " ")
     listitem=xbmcgui.ListItem(title, iconImage=thumb)
-    listitem.setInfo(type='music', infoLabels={'title': title, 'artist': artist, 'album': album})
+    listitem.setInfo(type='music', infoLabels={'title': title, 'artist': artist, 'album': album, 'duration': duration})
     listitem.setProperty('IsPlayable', isPlayable)
     listitem.setProperty('IsLive', isLive)
     listitem.setPath(url)
     return xbmcplugin.addDirectoryItem(HANDLE, url=url, listitem=listitem, isFolder=isFolder)
 
 def add_main_menu():
-    listitem=xbmcgui.ListItem("Kanaler")
-    listitem.setInfo(type='music', infoLabels={ 'Title': "Kanaler"})
-    listitem.setPath('channels')
-    u = sys.argv[0] + "channels/"
+    listitem=xbmcgui.ListItem("Live")
+    listitem.setInfo(type='music', infoLabels={ 'Title': "Live"})
+    listitem.setPath('live')
+    u = sys.argv[0] + "live/"
     xbmcplugin.addDirectoryItem(HANDLE, url=u, listitem=listitem, isFolder=True)
     listitem=xbmcgui.ListItem("Program A-Ö")
     listitem.setInfo(type='music', infoLabels={ 'Title': "Program A-Ö"})
     listitem.setPath('program')
     u = sys.argv[0] + "programs/"
+    xbmcplugin.addDirectoryItem(HANDLE, url=u, listitem=listitem, isFolder=True)
+    listitem=xbmcgui.ListItem("Kanaler")
+    listitem.setInfo(type='music', infoLabels={ 'Title': "Kanaler"})
+    listitem.setPath('channel')
+    u = sys.argv[0] + "channels/"
     xbmcplugin.addDirectoryItem(HANDLE, url=u, listitem=listitem, isFolder=True)
     return xbmcplugin.endOfDirectory(HANDLE)
 
@@ -178,13 +198,17 @@ if (__name__ == "__main__" ):
     modes = MODE.split('/')
     activemode =  modes[len(modes) - 2]
     parentmode =  modes[len(modes) - 3]
-    if activemode == "programs" :
+    print MODE, activemode, parentmode, 'hoioi' 
+    if activemode == "allprograms":
         list_programs(MODE)
-    elif activemode == "channels" :
-        list_channels()
-    elif parentmode == "programs" :
-        list_program(activemode)
-    else :
+    elif activemode == "live":
+        list_live()
+    elif activemode == "channels":
+        list_channels(MODE)
+    elif parentmode == "channels" and activemode == 'programs':
+        list_channel_programs(MODE)
+    elif parentmode == "program":
+        list_broadcasts(activemode)
+   
+    else:
         add_main_menu()
-
-
